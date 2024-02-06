8df42146d01fe9b50cf683b78acf4c2b323adafc
mdmahedihassanshuvo <mdmahedihassanshuvo08@gmail.com>
2024-02-06 21:21:03 +0600

    add process section in home page

---
+++ src/Component/Process/Process.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Process.css";
+import { IoBulb } from "react-icons/io5";
+import { IoMdMail } from "react-icons/io";
+import { RiListSettingsFill } from "react-icons/ri";
+import { SiPytest } from "react-icons/si";
+
+const Process = () => {
+  return (
+    <div className="processSection text-black">
+      <h5 className="text-center fw-semibold  text-info ">Process</h5>
+      <h3 className="text-center fw-semibold ">
+        Process that moves <br />
+        things forward
+      </h3>
+      <div className="cardContainer">
+        <div className="cardItem p-4 border d-flex flex-column justify-content-center align-items-center">
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <p className="text-white bg-black fs-3 px-2 rounded-pill "><IoBulb /></p>
+            <p className="processTitle">Ideate</p>
+          </div>
+          <p className="processText">
+            The ideation process is a crucial phase in the design process where
+            creative thinking and brainstorming
+          </p>
+        </div>
+        <div className="cardItem p-4 border d-flex flex-column justify-content-center align-items-center">
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <p className="text-white bg-black fs-3 px-2 rounded-pill "><IoMdMail /></p>
+            <p className="processTitle">Reserach</p>
+          </div>
+          <p className="processText">
+            The ideation process is a crucial phase in the design process where
+            creative thinking and brainstorming
+          </p>
+        </div>
+        <div className="cardItem p-4 border d-flex flex-column justify-content-center align-items-center">
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <p className="text-white bg-black fs-3 px-2 rounded-pill "><RiListSettingsFill /></p>
+            <p className="processTitle">Create</p>
+          </div>
+          <p className="processText">
+            The ideation process is a crucial phase in the design process where
+            creative thinking and brainstorming
+          </p>
+        </div>
+        <div className="cardItem p-4 border d-flex flex-column justify-content-center align-items-center">
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <p className="text-white bg-black fs-3 px-2 rounded-pill "><SiPytest /></p>
+            <p className="processTitle">Testing</p>
+          </div>
+          <p className="processText">
+            The ideation process is a crucial phase in the design process where
+            creative thinking and brainstorming
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Process;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Banner from "../../Component/Banner/Banner";
 import Services from "../../Component/Services/Services";
 import AboutSection from "../../Component/AboutSection/AboutSection";
+import Process from "../../Component/Process/Process";
 
 const Home = () => {
   return (
     <div className="text-primary">
       <Banner />
       <Services />
-      <AboutSection/>
+      <AboutSection />
+      <Process />
     </div>
   );
 };
